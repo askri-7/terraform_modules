4977d3a06439368aed23f2df7b81948bad6f73d4
askri-7 <isra.askri@ensi-uma.tn>
2026-07-20 15:58:44 +0100

    add all

--- enviromnent/dev/varaibles.tf
+++ enviromnent/dev/varaibles.tf
@@ -3,6 +3,130 @@ variable "resource_group_name" {
   description = "the resource group name"
 }
 
+variable "storage_account_name" {
+  type = string
+  
+}
+# vnet vars
+variable "virtual_network_name" {
+    type = string
+    description = "Vnet name"  
+}
+
+
+variable "address_space" {
+    type = list(string)
+    description = "address space value"  
+}
+
+
+
+variable "ddos_protection_plan" {
+  type = object({
+    enable = bool
+    id     = string
+  })
+  default = null
+  description = "ddos_plan"
+}
+
+
+variable "dynamic_subnets" {
+    type = map(object({cidr_block = string
+                       security_rules =list(object({name = string 
+                                                    priority = number 
+                                                    direction                  = string
+                                                    access                     = string
+                                                    protocol                   = string
+                                                    source_port_range          = string
+                                                    destination_port_range     = string
+                                                    source_address_prefix      = string
+                                                    destination_address_prefix = string}))
+}))
+    description = "map of dynamic subnets security rule block as dynamic var"
+}
+
+# vm vars 
+ 
+
+variable "pub_ips" {
+     type = map(object({
+     public_ip_location = string
+     allocation= string
+     }))
+    description = "needed public ips"
+}
+
+variable "location" {
+     type = string
+     description = "location of both nic + vm"
+  
+}
+
+variable "nic_name" {
+  type = string
+  
+}
+
+variable "ip_conf" {
+     type = object({
+       name = string
+       allocation = string 
+     })
+     # validate inputs
+     validation {
+    condition     = contains(["Dynamic", "Static"], var.ip_conf.allocation)
+    error_message = "allocation must be Dynamic or Static."
+  }
+  
+}
+
+variable "virtual_machine_vars" {
+   type = object({
+     name = string
+     size = string
+     admin_username = string
+     computer_name = string
+   })
+}
+variable "os_disk" {
+     type = object({
+    name                 = string
+    caching              = string
+    storage_account_type = string
+  
+})
+   description = "os_disk configuration"
+}
+
+variable "source_image" {
+  type = object({
+    publisher = string
+    offer     = string
+    sku       = string
+    version   = string
+  })
+  description = "source image configuration"
+}
+
+
+
+variable "boot_diagnostics" {
+    type = object({
+    enabled              = bool
+    storage_account_uri  = optional(string)
+  })
+
+  default = {
+    enabled = false
+  }
+   description = "boot diagnostics configuration"
+}
+
+
+
+
+
 
 
 
---
+++ modules/VM/outputs.tf
@@ -0,0 +1,30 @@
+output "vm_id" {
+  description = "The ID of the virtual machine"
+  value       = azurerm_linux_virtual_machine.vm.id
+}
+
+output "vm_name" {
+  description = "The name of the virtual machine"
+  value       = azurerm_linux_virtual_machine.vm.name
+}
+
+output "nic_id" {
+  description = "The ID of the network interface"
+  value       = azurerm_network_interface.nic.id
+}
+
+output "private_ip_address" {
+  description = "The private IP address of the VM"
+  value       = azurerm_network_interface.nic.private_ip_address
+}
+
+#the first IP configuration
+output "public_ip_id" {
+  description = "The ID of the public IP attached to the NIC"
+  value       = azurerm_network_interface.nic.ip_configuration[0].public_ip_address_id
+}
+
+output "location" {
+  description = "The Azure region of the VM + NIC"
+  value       = azurerm_linux_virtual_machine.vm.location
+}
