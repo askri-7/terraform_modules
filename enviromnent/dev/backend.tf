8ab516aa0dd96dd6dc139e21ae54a3ab7d5b5b50
askri-7 <isra.askri@ensi-uma.tn>
2026-07-21 09:20:50 +0100

    add all

--- enviromnent/dev/backend.tf
+++ enviromnent/dev/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "isra-rg-01"
+    storage_account_name = "terrafstorageaccount01"
+    container_name       = "tfstate"
+    key                  = "dev.tfstate"
+    use_azuread_auth     = true
+  }
 }
