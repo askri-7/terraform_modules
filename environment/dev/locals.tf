5e184deacf3a3ab03781e8932ff3f6cf73bfe682
askri-7 <isra.askri@ensi-uma.tn>
2026-07-27 18:55:11 +0100

    1  VM for all

--- environment/dev/locals.tf
+++ environment/dev/locals.tf
@@ -3,11 +3,3 @@ locals {
   github_issuer_url     = "https://token.actions.githubusercontent.com"
 }
 
-locals {
-  vm_roles = {
-    frontend = { subnet_key = "frontend", has_public_ip = true }
-    backend  = { subnet_key = "backend",  has_public_ip = false }
-    redis    = { subnet_key = "redis",    has_public_ip = false }
-    database = { subnet_key = "database", has_public_ip = false }
-  }
-}
