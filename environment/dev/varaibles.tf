6308d82e3bc55df8951821340ceae26374f2feb4
askri-7 <isra.askri@ensi-uma.tn>
2026-07-27 19:21:51 +0100

    1  VM for all

--- environment/dev/varaibles.tf
+++ environment/dev/varaibles.tf
@@ -90,7 +90,7 @@ variable "virtual_machine_vars" {
 }
 variable "os_disk" {
   type = object({
-    name = string
+    name                 = string
     caching              = string
     storage_account_type = string
 
